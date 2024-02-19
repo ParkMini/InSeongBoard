bfe15a5ae2b3ad40ec683bd3983963f8f1d2c7b0
ParkMini <parkteammini@gmail.com>
2024-02-19 20:22:53 +0900

    에러 페이지 생성

---
+++ pages/404.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>잘못된 접근</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        html,
+        body {
+            width: 100%;
+            height: 100%;
+            background: url("./toothless.gif");
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+
+        #container {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        h1, h2 {
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="container">
+        <h1>이도저도 아무것도 아닌 그러하지 못한 그런 곳</h1>
+        <h2>404</h2>
+    </div>
+</body>
+
+</html>
